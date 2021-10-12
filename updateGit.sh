b2b8ed39c37c163b961fadf5479dcf35d7e8ed97
Tom <tom@Jons-iMac.local>
2021-10-12 11:44:50 -0400

    ”hello”

--- updateGit.sh
+++ updateGit.sh
@@ -1,3 +1,11 @@
+if [$1 -eq ""];
+then
+	echo "-- enter text:"
+	exit
+fi
 git add --all
+echo "-- commiting changes"
 git commit -m ”$1”
+echo "-- pushing changes"
 git push -u origin master
+echo "-- command finished -- page updated"

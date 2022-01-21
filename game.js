4be78cfbd976f236acbf6742ed095127395c6ff8
tte33 <90270208+tte33@users.noreply.github.com>
2022-01-21 13:08:17 -0500

    ”ResetButton”

--- game.js
+++ game.js
@@ -4,10 +4,21 @@ function tappedSquare(buttonID){
 	let button = document.getElementById(buttonID);
 	if(isCrossTurn){
 		button.className = "square cross";
+		button.innerHTML = "X"
 	} else {
 		button.className = "square nought";
+		button.innerHTML = "0"
 	}
 	
 	isCrossTurn = !isCrossTurn;
 	
+}
+
+function reset(){
+	let buttons = document.getElementsByClassName('square');
+
+			for (let i = 0; i < buttons.length; i++){
+				buttons[i].className = 'square'
+				buttons[i].
+			}
 }
